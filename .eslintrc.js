93487e59a9a10524fae3871396fba4f7df0e2be5
wolfy1339 <webmaster@wolfy1339.com>
2020-11-21 14:40:21 -0500

    build(lint): Simplify the ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,63 +12,15 @@ module.exports = {
         }
     },
     parser: "babel-eslint",
-    extends: ["eslint:recommended"],
+    extends: ["eslint:recommended", "@hellomouse"],
     rules: {
-        "require-jsdoc": [
-            "warn",
-            {
-                require: {
-                    FunctionDeclaration: true,
-                    MethodDefinition: true,
-                    ClassDeclaration: true,
-                    ArrowFunctionExpression: true,
-                    FunctionExpression: true
-                }
-            }
-        ],
-        "comma-dangle": [
-            "error",
-            "never"
-        ],
         "padded-blocks": [
             "error",
             {
                 blocks: "never"
             }
         ],
-        "arrow-parens": [
-            "error",
-            "as-needed"
-        ],
         "one-var": "off",
-        camelcase: "off",
-        "max-len": [
-            "error",
-            {
-                code: 120,
-                tabWidth: 4,
-                ignoreComments: true,
-                ignoreTrailingComments: true,
-                ignoreUrls: true
-            }
-        ],
-        "id-match": "off", // Google sets this
-        curly: "off", // Can't configure this enough
-        "block-spacing": [
-            "error",
-            "always"
-        ],
-        "brace-style": [
-            "error",
-            "1tbs",
-            {
-                allowSingleLine: true
-            }
-        ],
-        "object-curly-spacing": [
-            "error",
-            "always"
-        ],
         "space-unary-ops": [
             "error",
             {
@@ -82,29 +34,11 @@ module.exports = {
             { blankLine: "always", prev: ["const", "let"], next: "*" },
             { blankLine: "any", prev: ["const", "let"], next: ["const", "let"] }
         ],
-        eqeqeq: [
-            "warn",
-            "smart"
-        ],
-        "no-redeclare": [
-            "error",
-            {
-                builtinGlobals: true
-            }
-        ],
         "no-shadow": "error",
         "no-useless-return": "error",
-        "no-cond-assign": "error",
         "block-scoped-var": "error",
         "no-else-return": "error",
         "no-undef-init": "error",
-        "quote-props": [
-            "error",
-            "as-needed"
-        ],
-        semi: [
-            "error",
-            "always"
-        ]
     }
 };
+
