7c051266702a42eab2517dd4efaf3273bf758ee2
wolfy1339 <webmaster@wolfy1339.com>
2020-11-21 21:17:45 -0500

    Rewrite DisplayQuote using react-router and MUI pagination

--- src/DisplayQuote.js
+++ src/DisplayQuote.js
@@ -2,93 +2,58 @@ import React, { Component } from 'react';
 
 import Quote from './Quote.js';
 
-import { Button, Fab } from '@material-ui/core';
+import { Fab, Pagination } from '@material-ui/core';
 import CreateIcon from '@material-ui/icons/Create';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 
 /** Component to display quotes */
 class DisplayQuote extends Component {
-    state = { quotes: [] };
+    state = { quotes: { quotes: [] }, id: 1, last_page: 1, total_quotes: 1 };
 
     /**
-     * Returns page or quote number
-     * @return {object<string, string | number>}
+     * Constructor
+     * @param {RouteComponentProps} props
      */
-    getNumber() {
-      let suffix = document.location.href.split('/');
-
-      suffix = suffix[suffix.length - 1];
-
-      /* Possible suffix list:
-         * page_n - The nth page, starting from page 1
-         * quote_n - Quote with id n, starting from quote 1 */
-      if (suffix.startsWith('page_')) {
-        let n = suffix.split('page_')[1];
-
-        n = isNaN(n) ? 1 : +n;
-
-        return {
-          suffix: 'page_',
-          value: n
-        };
-      } else if (suffix.startsWith('quote_')) {
-        let n = suffix.split('quote_')[1];
-
-        n = isNaN(n) ? 1 : +n;
-
-        return {
-          suffix: 'quote_',
-          value: n
-        };
-      }
+    constructor(props) {
+      super();
+      this.state = { ...this.state, id: props.match.params.rID || 1 };
+      this.isSingleQuote = true;
     }
 
     /** Function that runs when the component is mounted */
-    componentDidMount() {
-      let data = this.getNumber();
-
-      this.is_single_quote = true;
-      this.currentPage = -1;
+    componentWillMount() {
+      let suffix = document.location.pathname.split('/').slice(-1)[0];
 
       /* Possible suffix list:
          * page_n - The nth page, starting from page 1
          * quote_n - Quote with id n, starting from quote 1 */
-      if (data.suffix.startsWith('page_')) {
-        let n = data.value;
-
-        this.is_single_quote = false;
-        this.currentPage = n;
-
-        fetch('/get_quote_page/' + n)
+      if (suffix.startsWith('page_') || suffix === '') {
+        this.isSingleQuote = false;
+        fetch('https://hellomouse.net/api/quotes/get_quote_page/' + this.state.id)
           .then(res => res.json())
-          .then(quotes => this.setState({ quotes }));
+          .then(quotes => this.setState({ ...this.state, quotes }));
 
         // Get total quotes in DB
-        fetch('/num_pages/')
+        fetch('https://hellomouse.net/api/quotes/num_pages/')
           .then(res => res.json())
           .then(res => {
-            this.total_quotes = res.count;
-            this.last_page = res.last_page;
+            this.setState({ ...this.state, lastPage: res.last_page, totalQuotes: res.count });
           });
-      } else if (data.suffix.startsWith('quote_')) {
-        let n = data.value;
-
+      } else if (suffix.startsWith('quote_')) {
         // Blame the way the state is setup
-        fetch('/get_quote/' + n)
+        fetch('https://hellomouse.net/api/quotes/get_quote/' + this.state.id)
           .then(res => res.json())
-          .then(quote => this.setState({ quotes: { quotes: [quote] } }) );
+          .then(quote => this.setState({ ...this.state, quotes: { quotes: [quote] } }) );
       }
     }
+
 
     /**
      * @return {React.ReactElement}
      */
     render() {
-      let currentPage = this.getNumber().value;
-      let prevPage = currentPage - 1;
-      let nextPage = currentPage + 1;
-
-      prevPage = prevPage <= 0 ? '' : prevPage;
+      let currentPage = this.state.id;
+      let lastPage = this.state.lastPage;
 
       return (
         <div>
@@ -97,31 +62,23 @@ class DisplayQuote extends Component {
               channel={quote.channel} timestamp={quote.created_on} />
           ) : ''}
 
-          {!this.is_single_quote ? (
+          {!this.isSingleQuote ? (
             <span>
-              <Button dense onClick={() => {
-                window.location.href = '/page_1';
-              }}>⮜</Button>
-              <Button dense onClick={() => {
-                window.location.href = '/page_' + prevPage;
-              }}>{prevPage}</Button>
-              <Button dense onClick={() => {
-                window.location.href = '/page_' + currentPage;
-              }}>{currentPage}</Button>
-              <Button dense onClick={() => {
-                window.location.href = '/page_' + nextPage;
-              }}>{nextPage}</Button>
-              <Button dense onClick={() => {
-                window.location.href = '/page_' + this.last_page;
-              }}>⮞</Button>
+              <Pagination color="primary" shape="rounded" showFirstButton showLastButton count={lastPage} page={currentPage}
+                onChange={
+                  (event, value) => {
+                    this.setState({ ...this.state, id: value });
+                    this.props.history.push(value);
+                  }
+                } />
             </span>
           ) : ''}
 
-          <Fab color="secondary" style={{ position: 'fixed' }} onClick={() => {
-            window.location.href = '/add_quote';
-          }}>
-            <CreateIcon />
-          </Fab>
+          <Link to="/add_quote">
+            <Fab color="secondary" style={{ position: 'fixed' }} className={'fab'}>
+              <CreateIcon />
+            </Fab>
+          </Link>
         </div>
       );
     }
